d0273c001728c54d738b88afce283813267a27d1
MariaSyrokvash <mariya.syrokvash@gmail.com>
2020-11-06 16:38:41 +0300

    feat: add deploy settings

---
+++ build/precache-manifest.f9b81d0beb514b0ac9e6e7d5e5b83c73.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d437b1844c6d57fefa74bb7c915bc29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "675237767753d23163e2",
+    "url": "/static/css/main.8a80a244.chunk.css"
+  },
+  {
+    "revision": "dff398a06a9542ec0be5",
+    "url": "/static/js/2.fba6319d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.fba6319d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "675237767753d23163e2",
+    "url": "/static/js/main.86c40e5a.chunk.js"
+  },
+  {
+    "revision": "355b045a765c2e4709b9",
+    "url": "/static/js/runtime-main.0ff819e4.js"
+  },
+  {
+    "revision": "f6c79f174ea3bb693da1a9190a7bae93",
+    "url": "/static/media/send.f6c79f17.svg"
+  }
+]);
