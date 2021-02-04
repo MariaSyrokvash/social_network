73a5d4f5971f430f1765157bf100309cb7e1fc0b
MariaSyrokvash <mariya.syrokvash@gmail.com>
2021-02-04 19:17:00 +0300

    feat: add hashrouter for gh-page

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,11 @@ import React from 'react';
 import app from './App.module.css';
 import Header from './components/Header/Header';
 import NavBar from './components/NavBar/NavBar';
-import {BrowserRouter, Route, withRouter} from 'react-router-dom';
+import {HashRouter, Route, withRouter} from 'react-router-dom';
 import News from './components/News/News';
 import Music from './components/Music/Music';
 import Settings from './components/Settings/Settings';
-// import DialogsContainer from './components/Dialogs/DialogsContainer';
 import UsersContainer from './components/Users/UsersContainer';
-// import ProfileContainer from './components/Profile/ProfileContainer';
 import Login from './components/Login/Login';
 import {connect, Provider} from 'react-redux';
 import {compose} from 'redux';
@@ -61,9 +59,9 @@ const AppContainer = compose(
 	connect(mapStateToProps, {initializeAppTC}))(App) as React.FunctionComponent<any>
 
 export const MainApp = () => {
-	return <BrowserRouter basename={process.env.PUBLIC_URL}>
+	return <HashRouter>
 		<Provider store={store}>
 			<AppContainer/>
 		</Provider>
-	</BrowserRouter>
+	</HashRouter>
 }
